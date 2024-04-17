f506a68195909cda39ab8bed5caccab1a062f8c2
Narione <ynhp3214@gmail.com>
2024-04-17 20:12:18 +0900

    240417-2

--- BasicWeb/src/main/java/board/BoardService.java
+++ BasicWeb/src/main/java/board/BoardService.java
@@ -10,23 +10,23 @@ public class BoardService {
 	public BoardService(DataSource dataSource) {
 		this.dao = new BoardDAO(dataSource);
 	}
-	
+	//모든내용 select
 	public List<BoardVO> getBoardList() {
 		return dao.getBoardList();
 	}
-	
+	//한개의 글 상세페이지
 	public BoardVO getBoard(int searchno) {
 		return dao.getBoard(searchno);
 	}
-	
+	//글쓰기
 	public int insertBoard(BoardVO vo) {
 		return dao.insertBoard(vo);
 	}
-	
+	//글 수정
 //	public void updateBoard(BoardVO vo) {
 //		dao.updateBoard(vo);
 //	}
-//	
+//	// 글 삭제
 //	public void deleteBoard(int deleteNo) {
 //		dao.deleteBoard(deleteNo);
 //	}
